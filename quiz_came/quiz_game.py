71415dba894b1618e7a307a2ef34c402218247e5
Rohan-R07 <rveda013@gmail.com>
2026-03-12 18:52:12 +0530

    quiz

---
+++ quiz_came/quiz_game.py
@@ -0,0 +1,8 @@
+score = 0
+
+q1 = input("Python creator? ")
+
+if q1.lower()== "guido van rossum":
+    score+=1
+
+print("Score:",score)
--- text_adventure/text_adventure.py
+++
@@ -1,8 +0,0 @@
-print("You are in a forest")
-
-choice = input("Go left or right?")
-
-if choice == "left":
-    print("you found treasure!")
-else :
-    print("A monster caught you!")
